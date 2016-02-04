752eb6320fd0c7d51bc28fc95b2dcdce74984c90
qiujinhuang <18932678@qq.com>
2016-02-04 10:32:23 +0800

    first commit

---
+++ testBoost/testBoost.cpp
@@ -0,0 +1,17 @@
+// testBoost.cpp : 定义控制台应用程序的入口点。
+//
+
+#include <boost/lambda/lambda.hpp>
+#include <iostream>
+#include <iterator>
+#include <algorithm>
+
+int main()
+{
+	using namespace boost::lambda;
+	typedef std::istream_iterator<int> in;
+
+	std::for_each(
+		in(std::cin), in(), std::cout << (_1 * 3) << " ");
+	std::cout << "";
+}
